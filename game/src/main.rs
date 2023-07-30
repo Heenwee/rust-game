6ea504f617710f063e6d286b8f0fdda996ddba13
Henwee <heenwee@gmail.com>
2023-07-31 00:11:23 +0200

    what is a struct :sob:

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,16 +1,23 @@
 use bevy::prelude::*;
 
+pub mod particles;
+use particles::*;
+
 const STARTING_POSITION: Vec3 = Vec3::new(0.0, 0.0, 0.0);
 const SQUARE_SCALE: Vec3 = Vec3::new(1.0, 1.0, 1.0); 
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup,
-            setup,
-        )
+        .add_systems(Startup,(
+            setup.before(particle_effects),
+            particle_effects,
+        ))
         .run();
 }
+
+#[derive(Component)]
+struct Player;
 
 fn setup (
     mut commands: Commands,
@@ -20,11 +27,17 @@ fn setup (
     // create camera
     commands.spawn(Camera2dBundle::default());
 
-    // load sprite
-    commands.spawn(
+    // load player
+    commands.spawn((
         SpriteBundle {
             texture: asset_server.load("sprites/square.png"),
             transform: Transform::from_translation(STARTING_POSITION).with_scale(SQUARE_SCALE),
             ..default()
-    });
+        },
+        Player,
+        Particle {
+            lifetime: 5.0,
+            ..default()
+        },
+    ));
 }
---
+++ game/src/particles.rs
@@ -0,0 +1,24 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+pub struct Particle {
+    pub lifetime: f32,
+    pub burst: i32
+}
+
+impl Default for Particle {
+    fn default() -> Self {
+        Particle {
+            lifetime: 0.0,
+            burst: 0,
+        }
+    }
+}
+
+pub fn particle_effects(
+    particle_query: Query<&Particle>
+) {
+    for particle in &particle_query {
+        println!("lifetime: {0}, burst: {1}", particle.lifetime, particle.burst)
+    }
+}
