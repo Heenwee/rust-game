84e145641ab3631979eafbcde51a247030931241
Henwee <heenwee@gmail.com>
2023-07-30 22:29:49 +0200

    a square :000

--- game/src/main.rs
+++ game/src/main.rs
@@ -1,3 +1,30 @@
+use bevy::prelude::*;
+
+const STARTING_POSITION: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+const SQUARE_SCALE: Vec3 = Vec3::new(1.0, 1.0, 1.0); 
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup,
+            setup,
+        )
+        .run();
+}
+
+fn setup (
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+) {
+    println!("starting...");
+    // create camera
+    commands.spawn(Camera2dBundle::default());
+
+    // load sprite
+    commands.spawn(
+        SpriteBundle {
+            texture: asset_server.load("sprites/square.png"),
+            transform: Transform::from_translation(STARTING_POSITION).with_scale(SQUARE_SCALE),
+            ..default()
+    });
 }
